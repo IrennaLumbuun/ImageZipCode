afebfbf16ffbe9c61e355f4269b6a1b51c7c3bf7
irenna lumbuun <irenna.lumbuun@vw.com>
2020-11-21 03:55:06 -0800

    Basic map

---
+++ front-end/src/components/Maps/Maps.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import './Maps.css';
+import PropTypes from 'prop-types';
+import 'leaflet/dist/leaflet.css';
+
+
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+// });
+
+class Maps extends React.Component {
+
+  static propTypes = {
+    items: PropTypes.array,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      default_location: {
+        lat: 34.01819,
+        lng: -118.50005,
+        name: "Enterprise Vision Technology"
+      },
+      zoom: 10,
+      locations: []
+    };
+  }
+
+  showMarker = (locations) => {
+    console.log(locations)
+    return (
+      locations.map(item => (
+        <Marker position={[item.lat, item.lng]}>
+          <Popup>
+            <p><em>{item.name}</em></p>
+          </Popup>
+        </Marker>
+      ))
+    );
+  }
+
+  renderDefault = () => {
+    var location = [this.state.default_location.lat, this.state.default_location.lng];
+    return (
+      <MapContainer className="map" zoom={this.state.zoom} center={location}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.showMarker([this.state.default_location])}
+      </MapContainer>
+    );
+  }
+
+  // renderLocation() {
+  //   return (
+  //     <MapContainer className="map" zoom={10} center={[this.state.filtered[0].coord[0],this.state.filtered[0].coord[1]]}>
+  //       <TileLayer
+  //         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //       />
+  //       {this.showMarker()}
+  //     </MapContainer>
+  //   );
+  // }
+
+  render() {
+    console.log(this.props)
+    return (
+      this.renderDefault()       
+    );
+  }
+};
+
+export default Maps;
+
