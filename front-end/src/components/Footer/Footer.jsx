6e323e290b55779224ca124770911377a66f1bf2
irenna lumbuun <irenna.lumbuun@vw.com>
2020-11-21 03:04:09 -0800

    [#2] Create footer

---
+++ front-end/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <footer id="main-footer">
+            <a href="https://github.com/IrennaLumbuun/ImageZipCode" target="_blank">Github Link</a>
+        </footer>
+    );
+}
+
+export default Footer;
